511a173d15f10afb79d4a8b6e7b32902d2062d96
Ameya Karkal <AKarkal@CampusLabs.com>
2015-04-07 12:33:06 -0400

    added unit tests

--- java/sort/src/base/MergeSort.java
+++ java/sort/src/base/MergeSort.java
@@ -1,4 +1,7 @@
 package base;
+
+import java.util.Arrays;
+
 public class MergeSort implements ISort{
 
 	private final int[] _input;
@@ -9,6 +12,52 @@ public class MergeSort implements ISort{
 
 	@Override
 	public int[] sort() {
-		return _input;
+		return sortRecursively(_input);
+	}
+	
+	protected int[] sortRecursively(int[] input){
+		int length = input.length;
+		
+		if(length == 1) return input;
+		
+		int mid = length / 2;
+		
+		int[] sortedFirstHalf = sortRecursively(split(input, 0, mid));
+		
+		int[] sortedSecondHalf = sortRecursively(split(input, mid, length));
+		
+		return merge(sortedFirstHalf, sortedSecondHalf);
+	}
+	
+	protected int[] split(int[] input, int from, int to){
+		return Arrays.copyOfRange(input, from, to);
+	}
+	
+	protected int[] merge(int[] first, int[] second){
+		int firstSourceLength = first.length;
+		
+		int secondSourceLength = second.length;
+		
+		int maxLength = Math.max(firstSourceLength, secondSourceLength);
+		
+		int[] output = new int[maxLength];
+		
+		for(int walker = 0; walker < maxLength; walker++)
+		{
+			if(firstSourceLength > walker && secondSourceLength > walker)
+			{
+				output[walker] = Math.min(first[walker], second[walker]);
+				continue;
+			}
+			
+			if(firstSourceLength > walker){
+				output[walker] = first[walker];
+				continue;
+			}
+			
+			output[walker] = second[walker];
+		}
+		
+		return output;
 	}
 }
--- java/sort/src/base/tests/MergeSortTests.java
+++ java/sort/src/base/tests/MergeSortTests.java
@@ -1,5 +1,7 @@
 package base.tests;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,5 +15,42 @@ public class MergeSortTests {
 		ISort algorithm = new MergeSort(new int[]{ 1, 2 });
 		
 		Assert.assertNotNull(algorithm);
+	}
+	
+	@Test
+	public void shouldSplitInput(){
+		int[] input = new int[] { 1, 2, 3 };
+		int from = 0;
+		int mid = input.length / 2;
+		int to = input.length;
+		
+		MergeSortDummy sort = new MergeSortDummy(input);
+		
+		System.out.println(Arrays.toString(sort.split(input, from, mid)));
+		System.out.println(Arrays.toString(sort.split(input, mid, to)));
+	}
+	
+	@Test
+	public void shouldMergeInputs(){
+		int[] first = new int[] { 1 };
+		int[] second = new int[] { 2, 3};
+		
+		MergeSortDummy sort = new MergeSortDummy(new int[]{});
+		System.out.println(Arrays.toString(sort.merge(first, second)));
+	}
+	
+	private class MergeSortDummy extends MergeSort{
+		public MergeSortDummy(int[] input) {
+			super(input);
+		}
+		
+		public int[] split(int[] input, int from, int to){
+			return super.split(input, from, to);
+		}
+		
+		public int[] merge(int[] first, int[] second){
+			return super.merge(first, second);
+		}
+		
 	}
 }
