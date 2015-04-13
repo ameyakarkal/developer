4efb98de2b880e1477bffe29032b0dc04f3e1acc
Ameya Karkal <ameyakarkal@gmail.com>
2015-04-13 03:59:57 -0400

    strategies to find if all characters are unique in a given string

--- java/crackingcode/src/arrays/HasUniqueCharacter.java
+++ java/crackingcode/src/arrays/HasUniqueCharacter.java
@@ -2,4 +2,31 @@ package arrays;
 
 public class HasUniqueCharacter {
 
+	public interface IStrategy{
+		
+	}
+	
+	public static class BruteForce implements IStrategy{
+		// O(n ^ 2)
+	}
+	
+	public static class UsingAsciiMap implements IStrategy{
+		
+		// assumption : the character set is ASCII
+		/*
+		 * create an array / directory map[256] of boolean
+		 * 	for each character c = str.charAt(i) 
+		 * 		if map[c] then return NOT UNIQUE
+		 * 		else continue;
+		 * 
+		 * 	return UNIQUE (true);
+		 */
+	}
+	
+	public static class SortAndCompare implements IStrategy{
+		//O(n logn) to sort the array
+		 /*
+		 * single pass through array. if neighboring elements are same, the character is not unique
+		 */
+	}
 }
