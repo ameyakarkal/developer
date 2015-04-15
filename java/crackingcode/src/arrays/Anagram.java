58a80e8e384623285fcbcaa1d46137d48dab60a4
Ameya Karkal <ameyakarkal@gmail.com>
2015-04-14 23:45:08 -0400

    anagram and reverse string

---
+++ java/crackingcode/src/arrays/Anagram.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Anagram {
+
+	private interface IStrategy{
+		
+	}
+	
+	private class SortingStrategy implements IStrategy{
+		/**
+		 * 01. check if lengths of two string are same
+		 * 02. sort the character of the string O(n log n)
+		 * 03. single pass to check if the strings are equal O(n)
+		 */
+	}
+	
+	private class MapOfCharacterStrategy implements IStrategy{
+		/**
+		 * count per characters in the first string
+		 * count per characters in the second string
+		 * if the map match then anagram
+		 */
+	}
+}
--- java/crackingcode/src/arrays/ReverseString.java
+++ java/crackingcode/src/arrays/ReverseString.java
@@ -2,4 +2,20 @@ package arrays;
 
 public class ReverseString {
 
+	private interface IStrategy{
+		
+	}
+	
+	public class InPlaceSwapStrategy implements IStrategy{
+		
+		/**
+		 * take a pointer to the last position of the string.
+		 * keep pointer to the first position of the string
+		 * while(head < tail){
+		 * 		temp = head
+		 * 		head = tail
+		 * 		tail = temp
+		 * }
+		 */
+	}
 }
