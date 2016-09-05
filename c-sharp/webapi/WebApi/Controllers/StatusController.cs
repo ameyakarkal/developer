8764939708caa03f18b1de65069f38bcd9206805
ameyakarkal <ameyakarkal@live.com>
2016-09-05 16:34:58 -0400

    webapi. owin

---
+++ c-sharp/webapi/WebApi/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("status")]
+    public class StatusController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Index()
+        {
+            return Ok("ok"); 
+        }
+    }
+}
---
+++ c-sharp/webapi/WebApi/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+
+            configuration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(configuration);
+        }
+    }
+}
