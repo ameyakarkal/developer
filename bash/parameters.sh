810f13c643d5fa456de1abd59cfa288c7c3efd3b
frictionless <ameyakarkal@gmail.com>
2021-02-01 13:30:22 -0500

    Create parameters.sh

---
+++ bash/parameters.sh
@@ -0,0 +1,13 @@
+# source script.sh --environment prod
+
+environment=${environment:dev}
+
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+  shift
+done
+
+echo $environment
